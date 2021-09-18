7d40bcbf955763cd8146d28bc6979f2c99c75fad
Movaffaq <movaffaq1985@gmail.com>
2021-09-18 20:50:21 +0200

    Update HydrateLAMMPS.py

--- Solvate/HydrateLAMMPS.py
+++ Solvate/HydrateLAMMPS.py
@@ -1,10 +1,32 @@
 import numpy as np
 
-def hydrate(solute,solvent,lx,ly,lz,d):
+def hydrate(solute,solvent,lx,ly,lz,d,check_dim=False,center=True):
     xcoor = solute.xloxhi
     ycoor = solute.yloyhi
     zcoor = solute.zlozhi
+    
+    if center == True:	
+        shiftx = (float(xcoor[0])+float(xcoor[1]))/2
+        shifty = (float(ycoor[0])+float(ycoor[1]))/2
+        shiftz = (float(zcoor[0])+float(zcoor[1]))/2
+    else :
+        shiftx = 0; shifty = 0; shiftz = 0;
 
+    if check_dim == True:
+        lxs = float(xcoor[1])-float(xcoor[0])
+        lys = float(ycoor[1])-float(ycoor[0])
+        lzs = float(zcoor[1])-float(zcoor[0])
+	
+        if lx < lxs:
+            print('Error: The x dimension of solvation box cannot be smaller than solute i.e. %.2f' % lxs)
+            exit()
+        if ly < lys:
+            print('Error: The y dimension of solvation box cannot be smaller than solute i.e. %.2f' % lxs)
+            exit()
+        if lz < lzs:
+            print('Error: The z dimension of solvation box cannot be smaller than solute i.e. %.2f' % lxs)
+            exit()
+    
     coordinates = solute.AtomsProperties.coordinates
 
     Matoms = solute.AtomsProperties.AllAtomsProperties
@@ -56,14 +78,14 @@ def hydrate(solute,solvent,lx,ly,lz,d):
         molimpropers[1] += Tim
     Tim += solvent.Timpropers
     
-    x = -lx / 2
-    while x < lx / 2:
+    x = shiftx - lx / 2
+    while x < shiftx + lx / 2:
         x += d
-        y = -ly / 2
-        while y < ly / 2:
+        y = shifty - ly / 2
+        while y < shifty + ly / 2:
             y += d
-            z = -lz / 2
-            while z < lz / 2:
+            z = shiftz - lz / 2
+            while z < shiftz + lz / 2:
                 z += d
                 dmin = 1e5
                 for lin1 in range(len(coordinates.T)):
