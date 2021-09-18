88b690f50767df4c3e7dec9035d67a56551e68c1
Movaffaq <movaffaq1985@gmail.com>
2021-09-18 17:35:40 +0200

    Update solvation.py

--- example/solvation.py
+++ example/solvation.py
@@ -1,1 +1,33 @@
+from ReadDataLAMMPS import *
+from ReplicateSystem import *
+from HydrateLAMMPS import *
+from WriteDataLAMMPS import *
 
+# reading lammps data file
+print('Reading lammps data file(s) ...')
+Solute = 'go_opls_long.data'
+Solvent = 'water_TIP4P_long.data'
+
+SoluteData = readdatalammps(Solute,'full')
+SolventData = readdatalammps(Solvent,'full')
+
+print('Importing lammps data file(s) is finished.')
+
+# replication of go for liquid crystal
+#datafile = 'mydata.lammps'
+#Replicate = 2,2,1
+#Init_Atoms,Init_Bonds, Init_Angles, Lx, Ly, Lz = main_replicate(datafile, Replicate)
+
+# solvation
+print('Initiat solvation that make take while ...')
+lx = 10 # size for the waterbox
+ly = 10 # size for the waterbox
+lz = 10 # size for the waterbox
+d = 2.8 # distance between solute and solvent
+solvated = hydrate(SoluteData,SolventData,lx,ly,lz,d)
+print('Solvating is finished.')
+
+# writing lammps data file
+print('Exporting lammps data file.')
+name = 'test.data'
+write(solvated,name)
